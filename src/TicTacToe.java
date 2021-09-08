a59ce8064630c35bc1521901b888f87cf8cd3389
Isabel Martínez <i.m.martinez@hotmail.es>
2021-09-08 11:35:53 +0200

    player movement

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,15 @@
 public class TicTacToe {
+    private String[][] position = new String[3][3];
+    private String currentPlayer = "x";
+
+    public void play(int row, int column) {
+        if (this.position[row][column] == null) {
+            this.position[row][column] = this.currentPlayer;
+            this.currentPlayer = this.currentPlayer.equals("x") ? "o" : "x";
+        }
+    }
+
+    public String[][] getPosition() {
+        return this.position;
+    }
 }
--- test/TicTacToeTest.java
+++ test/TicTacToeTest.java
@@ -1,2 +1,59 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TicTacToeTest {
+
+    @Test
+    public void save_position_as_x_after_firs_move() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play(0,0);
+        String[][] mockPositions = new String[3][3];
+        mockPositions[0][0] = "x";
+        assertEquals(mockPositions, ticTacToe.getPosition());
+    }
+
+    @Test
+    public void save_position_as_x_after_two_moves() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play(0,0);
+        ticTacToe.play(1,0);
+        String[][] mockPositions = new String[3][3];
+        mockPositions[0][0] = "x";
+        mockPositions[1][0] = "o";
+        assertEquals(mockPositions, ticTacToe.getPosition());
+    }
+
+    @Test
+    public void save_position_as_after_three_moves() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play(0,0);
+        ticTacToe.play(1,0);
+        ticTacToe.play(0,1);
+        String[][] mockPositions = new String[3][3];
+        mockPositions[0][0] = "x";
+        mockPositions[1][0] = "o";
+        mockPositions[0][1] = "x";
+        assertEquals(mockPositions, ticTacToe.getPosition());
+    }
+
+    @Test
+    public void save_position_as_after_four_moves() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play(0,0);
+        ticTacToe.play(0,0);
+        String[][] mockPositions = new String[3][3];
+        mockPositions[0][0] = "x";
+        assertEquals(mockPositions, ticTacToe.getPosition());
+    }
+
+    @Test
+    public void save_position_as_after_four_moves() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play(0,0);
+        ticTacToe.play(0,0);
+        String[][] mockPositions = new String[3][3];
+        mockPositions[0][0] = "x";
+        assertEquals(mockPositions, ticTacToe.getPosition());
+    }
 }
