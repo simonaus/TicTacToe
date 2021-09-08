b4181e77416e0e6164c83b62ceda520f74352b89
Simon <simonrosenberg1@gmail.com>
2021-09-08 15:32:55 +0100

    Delete commented out code

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,45 +6,26 @@ public class TicTacToe {
     private GameStatus gameStatus = GameStatus.IN_PROGRESS;
 
     public void play(int row, int column) {
-            boolean isWon = false;
         if (this.board[row][column] == PositionStatus.EMPTY) {
             this.board[row][column] = this.currentPlayer;
-            if (check_for_win(row, column)) this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;;
-//            if (this.board[1][1] != PositionStatus.EMPTY) check_diagonals_for_win();
+            checkForWin(row, column);
             checkForDraw();
             this.currentPlayer = this.currentPlayer == PositionStatus.PLAYER_1 ? PositionStatus.PLAYER_2 : PositionStatus.PLAYER_1;
         }
     }
 
-//    private void check_diagonals_for_win() {
-//        if (this.board[0][0].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][2])) {
-//            this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;
-//        }
-//        if (this.board[0][2].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][0])) {
-//            this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;
-//        }
-//    }
-
-    private boolean check_for_win(int row, int column) {
-//        if (this.board[row][0].equals(this.board[row][1]) && this.board[row][1].equals(this.board[row][2])) {
-//            this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;
-//        }
-//        if (this.board[0][column].equals(this.board[1][column]) && this.board[1][column].equals(this.board[2][column])) {
-//            this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;
-//        }
-//        if (checkIf3PositionsMatch(new int[] {row, 0},new int[] {row, 1}, new int[] {row, 2})) return true;
-//        if (checkIf3PositionsMatch(new int[] {0, column},new int[] {1, column}, new int[] {2, column})) return true;
+    private void checkForWin(int row, int column) {
         boolean isRowMatch = checkIf3PositionsMatch(new int[] {row, 0},new int[] {row, 1}, new int[] {row, 2});
         boolean isColumnMatch = checkIf3PositionsMatch(new int[] {0, column},new int[] {1, column}, new int[] {2, column});
         boolean isBackwardsDiagonalMatch = false;
         boolean isForwardsDiagonalMatch = false;
         if (this.board[1][1] != PositionStatus.EMPTY) {
-//            if (checkIf3PositionsMatch(new int[]{0, 0}, new int[]{1, 1}, new int[]{2, 2})) return true;
-//            if (checkIf3PositionsMatch(new int[]{0, 2}, new int[]{1, 1}, new int[]{2, 0})) return true;
             isBackwardsDiagonalMatch = checkIf3PositionsMatch(new int[]{0, 0}, new int[]{1, 1}, new int[]{2, 2});
             isForwardsDiagonalMatch = checkIf3PositionsMatch(new int[]{0, 2}, new int[]{1, 1}, new int[]{2, 0});
         }
-        return (isRowMatch || isColumnMatch || isBackwardsDiagonalMatch || isForwardsDiagonalMatch);
+        if (isRowMatch || isColumnMatch || isBackwardsDiagonalMatch || isForwardsDiagonalMatch) {
+            this.gameStatus = this.currentPlayer == PositionStatus.PLAYER_1 ? GameStatus.PLAYER_1_WINS : GameStatus.PLAYER_2_WINS;
+        }
     }
 
     private boolean checkIf3PositionsMatch(int[] position1, int[] position2 ,int[] position3) {
